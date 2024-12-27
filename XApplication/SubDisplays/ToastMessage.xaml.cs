2f6ecdf1b8bb06b7087f77b7538107b962244210
MaxPra <epiccmax@gmx.at>
2024-12-27 12:54:34 +0100

    Fixed a bug with the little popup window when pressing monitoring key

--- XApplication/SubDisplays/ToastMessage.xaml.cs
+++ XApplication/SubDisplays/ToastMessage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -28,6 +29,16 @@ namespace GameTimeX
         private string message = "";
         private string title = "";
 
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        private const int GWL_EXSTYLE = -20;
+        private const int WS_EX_NOACTIVATE = 0x08000000;
+        private const int WS_EX_TOOLWINDOW = 0x00000080;
+
         public ToastMessage(string title, string message)
         {
             InitializeComponent();
@@ -37,6 +48,16 @@ namespace GameTimeX
 
             this.Left = SystemParameters.PrimaryScreenWidth - this.Width;
             this.Top = 0;
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            // Fenster so einstellen, dass es keinen Fokus erhält
+            IntPtr hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+            int exStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            SetWindowLong(hwnd, GWL_EXSTYLE, exStyle | WS_EX_NOACTIVATE | WS_EX_TOOLWINDOW);
         }
 
         /// <summary>
